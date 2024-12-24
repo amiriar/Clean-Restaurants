b9c3c112e0152d56de51b71d35150cf15c0c1bf1
amiriar <amiriar.irr@gmail.com>
2024-12-24 12:33:02 +0330

    address, dish and restaurant Entities added

---
+++ Clean-Restaurants.Domain/Entities/Restaurant.cs
@@ -0,0 +1,17 @@
+﻿namespace Clean_Restaurants.Domain.Entities
+{
+    public class Restaurant
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public bool HasDelivery { get; set; }
+
+        public string? Email { get; set; }
+        public string? ContactNumber { get; set; }
+
+        public Address? Address { get; set; }
+        public List<Dish>? Dishes { get; set; } = new();
+    }
+}
