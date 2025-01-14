e3cab0ff539e0b3639792d9c71d026189794c957
amiriar <amiriar.irr@gmail.com>
2025-01-14 13:17:36 +0330

    Add RestuarantsController with basic API setup

---
+++ Clean-Restaurants/Controllers/RestuarantsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clean_Restaurants.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class RestuarantsController : ControllerBase
+{
+}
