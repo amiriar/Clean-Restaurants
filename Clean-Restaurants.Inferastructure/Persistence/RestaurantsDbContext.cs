2b50482b5b32b33235ef322211b324fee5272caa
amiriar <amiriar.irr@gmail.com>
2025-01-14 13:17:19 +0330

    Refactor RestaurantsDbContext: streamline constructor, update DbSet properties, and enhance model configuration for Restaurant and Dish entities.

--- Clean-Restaurants.Inferastructure/Persistence/RestaurantsDbContext.cs
+++ Clean-Restaurants.Inferastructure/Persistence/RestaurantsDbContext.cs
@@ -1,19 +1,21 @@
 ﻿using Clean_Restaurants.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Clean_Restaurants.Inferastructure.Persistence
+namespace Clean_Restaurants.Inferastructure.Persistence;
+
+public class RestaurantsDbContext : DbContext
 {
-    public class RestaurantsDbContext: DbContext
-    {
-        public RestaurantsDbContext(DbContextOptions<RestaurantsDbContext> dbContextOptions): base(dbContextOptions) { }
-        
-        internal DbSet<Restaurant> Restaurants {  get; set; }
-        internal DbSet<Dish> Dishes { get; set; }
+    public RestaurantsDbContext(DbContextOptions<RestaurantsDbContext> options)
+        : base(options) { }
 
+    public DbSet<Restaurant> Restaurants { get; set; }
+    public DbSet<Dish> Dishes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Restaurant>().OwnsOne(r => r.Address);
+        modelBuilder.Entity<Restaurant>().HasMany(r => r.Dishes).WithOne().HasForeignKey(d => d.RestaurantId);
     }
 }
