27c95a3b726655268511e7d36e74c83d5b11ce1e
amiriar <amiriar.irr@gmail.com>
2025-01-18 09:33:27 +0330

    Add RestaurantsSeeder to initialize restaurant data in the database
    This commit introduces a new RestaurantsSeeder class that seeds the Restaurants table with initial data if the database is connected and empty. The seeder includes sample restaurants such as KFC and McDonald's, along with their details and associated dishes. This enhancement supports the application's data initialization process.

---
+++ Clean-Restaurants.Inferastructure/Seeders/RestaurantsSeeder.cs
@@ -0,0 +1,73 @@
+﻿using Clean_Restaurants.Domain.Entities;
+using Clean_Restaurants.Inferastructure.Persistence;
+
+namespace Clean_Restaurants.Inferastructure.Seeders;
+
+internal class RestaurantsSeeder(RestaurantsDbContext dbContext): IRestaurantsSeeder
+{
+    public async Task Seed()
+    {
+        if (await dbContext.Database.CanConnectAsync())
+        {
+            if (dbContext.Restaurants.Any())
+            {
+                var restuarants = GetRestuarants();
+                dbContext.Restaurants.AddRange(restuarants);
+                await dbContext.SaveChangesAsync();
+            }
+        }
+    }
+    private IEnumerable<Restaurant> GetRestuarants()
+    {
+        List<Restaurant> restaurants = [
+            new()
+            {
+                Name = "KFC",
+                Category = "Fast Food",
+                Description =
+                    "KFC (short for Kentucky Fried Chicken) is an American fast food restaurant chain headquartered in Louisville, Kentucky, that specializes in fried chicken.",
+                Email = "contact@kfc.com",
+                HasDelivery = true,
+                Dishes =
+                [
+                    new ()
+                    {
+                        Name = "Nashville Hot Chicken",
+                        Description = "Nashville Hot Chicken (10 pcs.)",
+                        Price = 10.30M,
+                    },
+
+                    new ()
+                    {
+                        Name = "Chicken Nuggets",
+                        Description = "Chicken Nuggets (5 pcs.)",
+                        Price = 5.30M,
+                    },
+                ],
+                Address = new ()
+                {
+                    City = "London",
+                    Street = "Cork St 5",
+                    PostalCode = "WC2N 5DU"
+                }
+            },
+            new ()
+            {
+                Name = "McDonald",
+                Category = "Fast Food",
+                Description =
+                    "McDonald's Corporation (McDonald's), incorporated on December 21, 1964, operates and franchises McDonald's restaurants.",
+                Email = "contact@mcdonald.com",
+                HasDelivery = true,
+                Address = new Address()
+                {
+                    City = "London",
+                    Street = "Boots 193",
+                    PostalCode = "W1F 8SR"
+                }
+            }
+        ];
+
+        return restaurants;
+    }
+}
